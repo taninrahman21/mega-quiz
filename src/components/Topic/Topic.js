f96b766d3378a66e23c64f165175a7b7978b72e2
Tanin Rahman <taninr87@gmail.com>
2022-10-11 02:01:09 +0600

    add topics section

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Topic = ({topic}) => {
+  console.log(topic)
   const {logo, name } = topic;
   return (
-    <div>
-      <img src={logo} alt="" />
-      <h2>{name}</h2>
+    <div className='bg-gray-200 flex flex-col border px-2 py-4 border-gray-300 rounded-md m-5'>
+      <img className='w-2/5' src={logo} alt="" />
+      <div>
+        <h2 className='text-2xl font-semibold'>{name}</h2>
+      </div>
+        <button className="py-2 px-5 bg-blue-800 rounded-md text-white mt-3">Start Quiz</button>
     </div>
   );
 };
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import {useLoaderData} from "react-router-dom";
-import Topic from '../Topic/Topic';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Topic from "../Topic/Topic";
 
 const Topics = () => {
-  const {data} = useLoaderData();
-  console.log(data);
+  const { data } = useLoaderData();
   return (
     <div>
-      <h1>Select The Topics</h1>
-      {
-        data.map((topic, idx) => <Topic key={idx} topic={topic}></Topic>)
-      }
+      <h1 className="text-3xl text-blue-700 text-center
+      mt-5 font-bold">Select The Quiz Topics</h1>
+      <div className="grid grid-cols-2 gap-20px items-center w-2/5 mx-auto my-10">
+        {data.map((topic, idx) => (
+          <Topic key={idx} topic={topic}></Topic>
+        ))}
+      </div>
     </div>
   );
 };
